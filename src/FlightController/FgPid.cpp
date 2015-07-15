dace3a54f5f7c95cae1e301151168a974386f6d1
Alex <alex@nls.la>
2015-07-15 16:26:45 +0300

    Pid improved.

--- src/FlightController/FgPid.cpp
+++ src/FlightController/FgPid.cpp
@@ -5,46 +5,62 @@
  *
  * @author Oleksii Aliakin (alex@nls.la)
  * @date Created Jul 14, 2015
- * @date Modified Jul 14, 2015
+ * @date Modified Jul 15, 2015
  */
 
 #include "FgPid.h"
 
 #include <QDebug>
 
-FgPid::FgPid(double initial_kp, double initial_kd, double initial_ki):
+FgPid::FgPid(double initial_kp, double initial_kd, double initial_ki, double initial_int_max):
     _kp(initial_kp),
     _kd(initial_kd),
-    _ki(initial_ki)
+    _ki(initial_ki),
+    _int_max(initial_int_max)
 {
 
 }
 
 double FgPid::update(double error)
 {
+    double p;
+    double i;
+    double d;
+
+    // proportional term
+    p = _kp * error;
+
     // integration
-    int_error += (error * _dt);
-    // limit
+    _int_error += _ki * error * _dt;
+    _int_error = fgap::math::limit(_int_error, _int_max);
+    i = _int_error;
 
     // differentiation
-    double diff = ((error - prev_error) / _dt);
+    if (isnan(_prev_d))
+    {
+        d = 0;
+        _prev_d = 0;
+    }
+    else
+        d = (error - _prev_error) / _dt;
 
-    double p = (_kp * error);
-    double i = (_ki * int_error);
-    double d = (_kd * diff);
+    double RC = 1 / (2 * M_PI * _filter);
+    d = _prev_d + ((_dt / (RC + _dt)) * (d - _prev_d));
 
-    control = p + i + d;
+    _prev_d = d;
+    _prev_error = error;
+    d = _kd * d;
 
-    qDebug() << "Error = " << error << "Control = " << control << "(i = " << i << ")";
-    prev_error = error;
+    _control = p + i + d;
 
-    return control;
+//    qDebug() << "Error = " << error << "Control = " << _control << "(i = " << i << ")";
+    return _control;
 }
 
 void FgPid::reset()
 {
-    prev_error = 0;
-    int_error = 0;
+    _prev_error = 0;
+    _int_error = 0;
 }
 
 
--- src/FlightController/FgPid.h
+++ src/FlightController/FgPid.h
@@ -5,16 +5,18 @@
  *
  * @author Oleksii Aliakin (alex@nls.la)
  * @date Created Jul 14, 2015
- * @date Modified Jul 14, 2015
+ * @date Modified Jul 15, 2015
  */
 
 #ifndef FGPID_H
 #define FGPID_H
 
+#include "FgMath.h"
+
 class FgPid
 {
 public:
-    FgPid(double initial_kp, double initial_kd, double initial_ki);
+    FgPid(double initial_kp, double initial_kd, double initial_ki, double initial_int_max = 10);
     double update(double error);
     void reset();
 
@@ -23,9 +25,14 @@ private:
     double _kd = 0.0;
     double _ki = 0.0;
     double _dt = 1;
-    double prev_error = 0;
-    double int_error = 0;
-    double control = 0;
+
+    double _int_max = 10;
+
+    double _prev_error = 0;
+    double _prev_d = NAN;
+    double _int_error = 0;
+    double _control = 0;
+    int _filter = 20;
 };
 
 #endif // FGPID_H
