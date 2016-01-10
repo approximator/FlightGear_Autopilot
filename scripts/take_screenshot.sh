89a1fa4692931b38bbe53a176c6ddb47a7bf78ab
Alex <alex@nls.la>
2016-01-10 19:31:53 +0100

    Test deployed application in another container.
    (fixes #16)
    
    Change-Id: Ia9ac9f93161949a1c9e9bd5578d7513dd1cd28ab

--- scripts/take_screenshot.sh
+++ scripts/take_screenshot.sh
@@ -4,16 +4,21 @@ COMMAND=$1
 SCREENSHOT_FILE=$2
 
 echo "Command = $COMMAND"
+
+Xvfb :5 -screen 0 1280x1024x24 &
+XVFB_PID=$!
+
 sleep 2
 # run command
-$COMMAND &
+DISPLAY=:5 $COMMAND &
 COMMAND_PID=$!
 # wait
 sleep 60
 
 # take screenshot
-import -window root "${SCREENSHOT_FILE}"
+DISPLAY=:5 import -window root "${SCREENSHOT_FILE}"
 
 sleep 2
 kill -15 $COMMAND_PID
 sleep 2
+kill -15 $XVFB_PID
