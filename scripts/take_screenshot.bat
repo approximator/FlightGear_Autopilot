ede8605092049ba9a15a4b1a74006c3e49703243
Oleksii Aliakin <alex@nls.la>
2016-06-19 20:47:32 +0200

    Removed unnecessary scripts.
    Signed-off-by: Oleksii Aliakin <alex@nls.la>

--- scripts/take_screenshot.bat
+++
@@ -1,9 +0,0 @@
-
-start fgautopilot-0.0.3/fgautopilot.exe
-
-curl -fsS -o nircmd.zip http://www.nirsoft.net/utils/nircmd.zip
-7z x nircmd.zip
-dir
-nircmd.exe cmdwait 120000 savescreenshot "shot.png"
-dir
-taskkill /f /im "fgautopilot.exe"
